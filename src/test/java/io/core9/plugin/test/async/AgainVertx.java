8d0f075ff9c4dcfee5430f92d5a91d8c7e99d79b
jessec <robertjessecooke@gmail.com>
2014-06-06 23:04:30 +0200

    adding mod-rxvertx

--- src/test/java/io/core9/plugin/test/async/AgainVertx.java
+++ src/test/java/io/core9/plugin/test/async/AgainVertx.java
@@ -1,11 +1,17 @@
 package io.core9.plugin.test.async;
 
+import io.vertx.rxcore.java.eventbus.RxEventBus;
+import io.vertx.rxcore.java.eventbus.RxMessage;
+
 import org.junit.Test;
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.Vertx;
 import org.vertx.java.core.VertxFactory;
 import org.vertx.java.core.eventbus.EventBus;
 import org.vertx.java.core.eventbus.Message;
+
+import rx.Observable;
+import rx.functions.Action1;
 
 public class AgainVertx {
 
@@ -18,16 +24,48 @@ public class AgainVertx {
 	public void testVertx() {
 
 		Handler<Message> myHandler = new Handler<Message>() {
-		    public void handle(Message message) {
-		        System.out.println("I received a message " + message.body());
-		    }
+			public void handle(Message message) {
+				System.out.println("I received a message " + message.body());
+			}
 		};
 
 		eb.registerHandler("test.address", myHandler);
-		
+
 		eb.publish("test.address", "hello world");
-		
+
 		System.out.print("pause" + "\n");
 
 	}
+
+	@Test
+	public void testEventbusRx() {
+
+		RxEventBus rxEventBus = new RxEventBus(vertx.eventBus());
+		rxEventBus.<String> registerHandler("foo").subscribe(
+				new Action1<RxMessage<String>>() {
+					public void call(RxMessage<String> message) {
+						
+						System.out.println("Message is : "
+								+ message.coreMessage().body());
+						// Send a single reply
+						message.reply("pong!");
+					}
+				});
+
+		Observable<RxMessage<String>> obs = rxEventBus.send("foo", "ping!");
+
+		obs.subscribe(new Action1<RxMessage<String>>() {
+			public void call(RxMessage<String> message) {
+				// Handle response
+				System.out.println("Message is : "
+						+ message.coreMessage().body());
+			}
+		}, new Action1<Throwable>() {
+			public void call(Throwable err) {
+				// Handle error
+			}
+		});
+
+	}
+
 }
