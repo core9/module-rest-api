49035ee5ce084c5db98637d6425698613afb4534
jessec <robertjessecooke@gmail.com>
2014-06-07 10:13:09 +0200

    all test passing!

--- src/test/java/io/core9/plugin/test/async/AgainVertx.java
+++ src/test/java/io/core9/plugin/test/async/AgainVertx.java
@@ -1,19 +1,30 @@
 package io.core9.plugin.test.async;
 
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import io.vertx.rxcore.java.RxVertx;
 import io.vertx.rxcore.java.eventbus.RxEventBus;
 import io.vertx.rxcore.java.eventbus.RxMessage;
 
 import org.junit.Test;
+import org.vertx.java.core.Context;
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.Vertx;
 import org.vertx.java.core.VertxFactory;
 import org.vertx.java.core.eventbus.EventBus;
 import org.vertx.java.core.eventbus.Message;
+import org.vertx.testtools.TestVerticle;
 
 import rx.Observable;
+import rx.Observer;
 import rx.functions.Action1;
+import rx.functions.Func2;
+import static org.vertx.testtools.VertxAssert.*;
 
-public class AgainVertx {
+import org.junit.Test;
+
+public class AgainVertx extends TestVerticle {
 
 	private Vertx vertx = VertxFactory.newVertx();
 
@@ -34,7 +45,7 @@ public class AgainVertx {
 		eb.publish("test.address", "hello world");
 
 		System.out.print("pause" + "\n");
-
+		 testComplete();
 	}
 
 	@Test
@@ -65,7 +76,10 @@ public class AgainVertx {
 				// Handle error
 			}
 		});
-
+		 testComplete();
 	}
+	
+	
+
 
 }
