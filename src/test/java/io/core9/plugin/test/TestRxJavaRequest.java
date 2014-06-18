4833c8da873a734486cbc704edf8c2c1f85ad217
jessec <robertjessecooke@gmail.com>
2014-06-18 08:13:55 +0200

    test setup

--- src/test/java/io/core9/plugin/test/TestRxJavaRequest.java
+++ src/test/java/io/core9/plugin/test/TestRxJavaRequest.java
@@ -1,7 +1,13 @@
 package io.core9.plugin.test;
 
+import static org.junit.Assert.assertNotNull;
+import io.core9.core.PluginRegistry;
+import io.core9.core.PluginRegistryImpl;
+import io.core9.core.boot.BootstrapFramework;
 import io.core9.plugin.rest.RestRequest;
 import io.core9.plugin.rest.RestRequestImpl;
+import io.core9.plugin.rest.RestRouter;
+import io.core9.plugin.rest.RestRouterImpl;
 import io.core9.plugin.server.VirtualHost;
 import io.core9.plugin.server.request.Method;
 
@@ -9,6 +15,17 @@ import java.util.Map;
 
 public class TestRxJavaRequest {
 
+	
+	private  PluginRegistry registry;
+	private  RestRouter restRouter;
+
+	public void setUp() {
+		BootstrapFramework.run();
+		registry = PluginRegistryImpl.getInstance();
+		restRouter = (RestRouter) registry.getPlugin(RestRouterImpl.class);
+
+		assertNotNull(restRouter);
+	}
 	
 	
 	//FIXME test this!!
@@ -35,12 +52,18 @@ public class TestRxJavaRequest {
 		Map<String, Object> result = EventBusUtils.getInstance().procesRequest(collection);
 		
 		
-		
+		 
 	}
 	
 	
 	
-	
+	public static void main(String[] args){
+		
+		TestRxJavaRequest testRxJavaRequest = new TestRxJavaRequest();
+		
+		testRxJavaRequest.setUp();
+		testRxJavaRequest.sendRequest();
+	}
 	
 	
 	
