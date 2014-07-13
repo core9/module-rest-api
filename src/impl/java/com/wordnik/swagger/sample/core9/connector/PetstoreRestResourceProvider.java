d95d104a4c03e01a2b04272a515c37d547ec2fab
jessec <robertjessecooke@gmail.com>
2014-07-13 13:34:30 +0200

    Provider needed own interface that extends restresourceprovider

---
+++ src/impl/java/com/wordnik/swagger/sample/core9/connector/PetstoreRestResourceProvider.java
@@ -0,0 +1,7 @@
+package com.wordnik.swagger.sample.core9.connector;
+
+import io.core9.plugin.rest.RestResourceProvider;
+
+public interface PetstoreRestResourceProvider extends RestResourceProvider {
+
+}
